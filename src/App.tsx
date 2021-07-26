93cec403c35a0413a7b972492ce492c42aab97fe
Robert Brennan <robert.brennan@analog.com>
2021-07-26 13:53:51 -0400

    working on result display

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function CryptoPunks({ cryptoPunks }: {
   const { error, loading, data } = useQuery(gql`
     {
       nfts(where: {type: ${type}}) {
+      # nfts(where: {id: 8192}) {
         id
         assignedTo {
           id
@@ -27,14 +28,57 @@ function CryptoPunks({ cryptoPunks }: {
   
   return (
     <div style={{alignItems: "center", justifyContent: "center", padding: '20px'}}>
-      <div style={{width: '100%', textAlign: 'center'}}>
-        <select style={{fontSize: '20px', padding: '10px'}} onChange={(e) => setType(e.currentTarget.value)}>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select><br/><br/>
+
+      <div style={{flex: 1, flexDirection: 'row', display: 'flex', borderStyle: 'solid', borderWidth: '1px', padding: 20}}>
+
+        <div style={{flex: 1, textAlign: 'left', }}>
+          <h1>CryptoPunks Browser</h1>
+        </div>
+
+        <div style={{textAlign: 'left', flex: 2, alignSelf: 'center'}}>
+          <h3>Select type: <select style={{fontSize: '20px', padding: '10px'}} onChange={(e) => setType(e.currentTarget.value)}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="alien">Alien</option>
+            <option value="ape">Ape</option>
+            <option value="zombie">Zombie</option>
+          </select></h3>
+        </div>
+
       </div>
 
-      {(error || loading) ? 'Loading...' : JSON.stringify(data)}
+      {(error || loading) ? (
+        <blockquote><br/><br/>Loading...</blockquote>
+        ) : (
+          (data as any).nfts.map((n: any, i: number) => {
+            return (
+              <div key={`nft-index-${i}`} style={{
+                marginRight: 'auto',
+                marginLeft: 'auto',
+                padding: 10,
+                margin: 20,
+                borderWidth: '1px',
+                borderStyle: 'solid',
+                flexDirection: 'row',
+                display: 'flex'
+              }}>
+                <div style={{flex: 1, padding: 10, alignSelf: 'center'}}>
+                  CryptoPunk id: <code>{n.id.toString()}</code>
+                </div>
+
+                <div style={{flex: 3, padding: 10}}>
+                  Accessories:<br/>{n.accessories.join(', ')}
+                </div>
+
+              </div>
+            )
+          })
+        )
+      }
+
+
+
+
     </div>
   )
 }
